b063c535e1ae65c85fe95e005645d2d17d07baca
atul24112001 <atulmorchhlay204@gmail.com>
2024-04-29 18:56:13 +0530

    Updated ws url

--- apps/client/src/hooks/useSocket.tsx
+++ apps/client/src/hooks/useSocket.tsx
@@ -4,7 +4,7 @@ const useSocket = () => {
   const [socket, setSocket] = useState<WebSocket | null>(null);
 
   useEffect(() => {
-    const ws = new WebSocket("wss://chess.atulmorchhlay.com");
+    const ws = new WebSocket("wss://ws-chess.atulmorchhlay.com");
     ws.onopen = () => {
       setSocket(ws);
     };
